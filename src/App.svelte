21738d8fb7787b567e66baf591ed19396e9715dc
Eternal <konstantinsteinmiller@gmail.com>
2019-06-04 23:03:17 +0200

    project setup svelte + tailwind + sass + axios

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+import '../assets/css/tailwind.css'
+export let name;
+</script>
+
+<style lang="sass">
+
+</style>
+
+<div class="bg-grey-dark p-4 gra-br-red">
+  <h1 class="text-blue text-xxs">Hello {name}!</h1>
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// import { sass } from 'svelte-preprocess-sass'
+const sass = require('svelte-preprocess-sass').sass
+console.log('sass: ', sass) // eslint-disable-line
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
+
+module.exports = {
+	entry: {
+		bundle: ['./src/main.js']
+	},
+	resolve: {
+		extensions: ['.mjs', '.js', '.svelte']
+	},
+	output: {
+		path: __dirname + '/public',
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						emitCss: true,
+						hotReload: true,
+						style: sass({ all: true })
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					/**
+					 * MiniCssExtractPlugin doesn't support HMR.
+					 * For developing, use 'style-loader' instead.
+					 * */
+					{ loader: prod
+							? MiniCssExtractPlugin.loader
+							: 'style-loader'
+					},
+					{ loader: 'css-loader'		},
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: [
+								require('tailwindcss'),
+								require('autoprefixer'),
+							],
+						},
+					}
+				]
+			}
+		]
+	},
+	mode,
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
+	devtool: prod ? false: 'source-map'
+};
